9dcda826efe9b756f97bb2d662a7b4ea86afcd8e
reiikz <reiikz@RZ-MAIN>
2024-12-17 20:32:12 -0300

    show cpanel

--- cpanel/cpanelSections/show.php
+++ cpanel/cpanelSections/show.php
@@ -18,30 +18,91 @@ redirectIfNotLoggedIn();
 */
 include_once $GLOBALS["webroot"] . "/core/parser.php";
 
-echo "<pre>";
 $available_zones = bind9_zoneconfig_decode($CONFIG["ZoneConfigFile"]);
-print_r($available_zones);
-$database = bind9_zonedb_decode($available_zones[$available_zones["zones"][0]]["file"]);
-print_r($database);
-// echo "\n\n";
-// echo bind9_zoneconfig_encode($available_zones);
-// echo "\n\n";
-// echo bind9_zonedb_encode($database);
-
-echo "</pre>";
 
 ?>
+<style>
+    .DNSrecord {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr min-content min-content;
+        margin: 1rem;
+        background-color: rgba(0,0,0,0.1);
+        padding: 1rem;
+        border-radius: 3px;
+    }
+    
+input[type="submit"]{
+    margin-left: 5px;
+}
+</style>
 
 <div class="content">
     <?php
         foreach($available_zones["zones"] as $zoneName){
             echo "<div class=\"DNSzone\">\n";
                 echo "<div class=\"DNSZone_Title\">$zoneName</div>\n";
+                
+                $zonedb = bind9_zonedb_decode($available_zones[$zoneName]["file"]);
 
-                // foreach()
+                foreach($zonedb["recordset"] as $recordName => $set){
+                    foreach($set["types"] as $recordType){
+                        // echo "<pre>";
+                        // print_r($set);
+                        // print_r($recordType);
+                        // echo "</pre>";
+                        foreach($set[$recordType] as $valueSet){
+                            $ttl = $valueSet["ttl"];
+                            echo "
+                                <div class=\"DNSrecord\">
+                                    <div>
+                                        $recordName
+                                    </div>
+                                    <div>
+                                        $ttl
+                                    </div>
+                                    <div>
+                                        $recordType
+                                    </div>
+                                    <div>
+                                        " . $valueSet["value"] . "
+                                    </div>
+                                    <div>
+                                        <form method=\"POST\" Action=\"" . getPathClientWebRoot() . "/API/record/delete.php\" >
+                                            <input type=\"submit\" value=\"Delete\" />
+                                            <input type=\"hidden\" name=\"record\" value=\"$recordName\"/>
+                                        </form>
+                                    </div>
+                                    <div>
+                                        <form method=\"POST\" Action=\"" . getPathClientWebRoot() . "cpanel/cpanelSections/updateRecord.php\" >
+                                            <input type=\"submit\" value=\"Edit\" />
+                                            <input type=\"hidden\" name=\"record\" value=\"$recordName\"/>
+                                        </form>
+                                    </div>
+                                </div>
+                            ";
+                        }
+                    }
+                }
 
             echo "</div>\n";
         }
         
     ?>
 </div>
+
+<?php
+
+// echo "<pre>";
+// $available_zones = bind9_zoneconfig_decode($CONFIG["ZoneConfigFile"]);
+// print_r($available_zones);
+// $database = bind9_zonedb_decode($available_zones[$available_zones["zones"][0]]["file"]);
+// print_r($database);
+// // echo "\n\n";
+// // echo bind9_zoneconfig_encode($available_zones);
+// // echo "\n\n";
+// // echo bind9_zonedb_encode($database);
+
+
+// echo "</pre>";
+
+?>
