80dc0d6bb2c9b87d12183f73bb8c4f81c8ff5157
reiikz <reiikz@RZ-MAIN>
2026-08-16 22:22:25 -0300

    edit section redirection broken

--- cpanel/cpanelSections/updateRecord.php
+++ cpanel/cpanelSections/updateRecord.php
@@ -74,7 +74,7 @@ $domain=$_GET["record"];
             Value: <input type="text" name="newValue" value="<?php echo $_GET["value"]; ?>" />
             TTL: <input type="text" name="newTTL" value="<?php echo $_GET["ttl"]; ?>" />
 
-            <input type="hidden" name="returnTo" value="<?php echo $_GET["returnTo"]; ?>/" />
+            <input type="hidden" name="returnTo" value="<?php echo $_GET["returnTo"]; ?>" />
             <input type="hidden" name="authority" value="<?php echo $_GET["authority"]; ?>" />
             <input type="submit" value="Update" />
         </form>
